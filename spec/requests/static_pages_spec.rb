11a0b93e3fbfe4938976dfb4b0917d3f486b4bba
Carl <carl@lift3d.co>
2014-05-08 10:10:34 -0400

    added full title_helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,9 @@
 require 'spec_helper'
 
 describe "Static pages" do
+
+  let(:base_title) {"Ruby on Rails Tutorial Sample App"}
+
   describe "Home page" do
     it "should have the content 'Sample App'" do
      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
@@ -11,7 +14,7 @@ describe "Static pages" do
     it "should have the title 'Home'" do
      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
       visit '/static_pages/home'
-      expect(page).to have_title('Ruby on Rails Tutorial Sample App | Home')
+      expect(page).to have_title('#{base_title} | Home')
     end
   end
 
@@ -24,20 +27,33 @@ describe "Static pages" do
     it "should have the title 'Help'" do
      	# Run the generator again with the --webrat flag if you want to use webrat methods/matchers
    	   	visit '/static_pages/help'
-	    expect(page).to have_title('Ruby on Rails Tutorial Sample App | Help')
+	    expect(page).to have_title('#{base_title} | Help')
     end
   end
 
   describe "About page" do
-  	it "should have the content 'About Us'" do
-  		visit '/static_pages/about'
-  		expect(page).to have_content('About Us')
+    it "should have the content 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_content('About Us')
+    end
+    it "should have the title 'About Us'" do
+
+      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+    visit '/static_pages/about'
+        expect(page).to have_title('#{base_title} | About Us')
+    end
+  end
+
+  describe "Contact page" do
+  	it "should have the content 'Contact Us'" do
+  		visit '/static_pages/Contact'
+  		expect(page).to have_content('Contact Us')
   	end
-  	it "should have the title 'About Us'" do
+  	it "should have the title 'Contact Us'" do
 
 	    # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-		visit '/static_pages/about'
-      	expect(page).to have_title('Ruby on Rails Tutorial Sample App | About Us')
+		visit '/static_pages/Contact'
+      	expect(page).to have_title('#{base_title} | Contact Us')
     end
   end
 end
